ac0a9e09c91588a037aed466cc6c8cced2f78818
Carrak <51882360+Carrak@users.noreply.github.com>
2020-06-09 01:17:02 +0400

    Refactoring and small updates

--- TeaBot/Modules/Exclusive.cs
+++ TeaBot/Modules/Exclusive.cs
@@ -7,16 +7,18 @@ using Discord.Commands;
 using Discord.WebSocket;
 using TeaBot.Attributes;
 using System.Text.RegularExpressions;
+using TeaBot.Preconditions;
 
 namespace TeaBot.Modules
 {
-    //[HelpCommandIgnore]
+    [HelpCommandIgnore]
     [Summary("Commands that can only be executed in specific servers")]
     public class Exclusive : InteractiveBase
     {
 
         [Command("quote", true)]
         [Exclusive(364771834325106689)]
+        [Ratelimit(5, Measure.Seconds)]
         public async Task Quote()
         {
             var channel = Context.Client.GetChannel(639860672666271806) as ISocketMessageChannel;
@@ -32,28 +34,10 @@ namespace TeaBot.Modules
                 foreach (var ping in pings)
                 {
                     string pingString = ping.ToString();
-                    text = text.Replace(pingString, $"@{Context.Client.GetUser(MentionUtils.ParseUser(pingString)).ToString()}");
+                    text = text.Replace(pingString, $"@{Context.Client.GetUser(MentionUtils.ParseUser(pingString))}");
                 }
 
-                return text;
-
-                /*
-                int pingStart = text.IndexOf("<@");
-                int pingEnd = text.IndexOf(">");
-
-                while (pingStart != -1 && pingEnd != -1)
-                {
-                    string toParse = text.Substring(pingStart, pingEnd - pingStart + 1);
-                    ulong id = MentionUtils.ParseUser(toParse);
-                    text = text.Replace(toParse, $"@{Context.Client.GetUser(id)}");
-
-                    pingStart = text.IndexOf("<@");
-                    pingEnd = text.IndexOf(">");
-                }
-
-                return text;
-                */
-                
+                return text;                
             }
         }
     }
