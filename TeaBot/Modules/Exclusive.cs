a0db3e722323920c55344dde6d4e8f0f7e00d074
Carrak <51882360+Carrak@users.noreply.github.com>
2020-06-13 02:44:41 +0400

    Reworked quote command

--- TeaBot/Modules/Exclusive.cs
+++ TeaBot/Modules/Exclusive.cs
@@ -3,12 +3,13 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord;
-using Discord.Addons.Interactive;
 using Discord.Commands;
 using Discord.WebSocket;
 using TeaBot.Attributes;
-using TeaBot.Preconditions;
 using TeaBot.Commands;
+using TeaBot.Preconditions;
+using TeaBot.Utilities;
+using TeaBot.Main;
 
 namespace TeaBot.Modules
 {
@@ -25,20 +26,21 @@ namespace TeaBot.Modules
             var messages = await channel.GetMessagesAsync().FlattenAsync();
             int randomIndex = new Random().Next(0, messages.Count());
             var quote = messages.ElementAt(randomIndex);
-            await ReplyAsync(ReplacePings(quote.Content) + "\n" + string.Join("\n", quote.Attachments.Select(x => x.Url)));
 
-            string ReplacePings(string text)
-            {
-                var pings = Regex.Matches(text, @"\<\@[\s\S]*?\>");
+            var embed = new EmbedBuilder();
 
-                foreach (var ping in pings)
-                {
-                    string pingString = ping.ToString();
-                    text = text.Replace(pingString, $"@{Context.Client.GetUser(MentionUtils.ParseUser(pingString))}");
-                }
+            embed.WithColor(TeaEssentials.MainColor)
+                .WithDescription(quote.Content)
+                .WithAuthor(quote.Author)
+                .WithFooter(quote.CreatedAt.DateTime.ToString("dd.MM.yyyy HH:mm:ss"));
 
-                return text;
-            }
+            if (quote.Attachments.Count() > 0)
+                embed.WithImageUrl(quote.Attachments.First().Url);
+            else if (quote.Embeds.Count() > 0)
+                embed.WithImageUrl(quote.Embeds.First().Url);
+            
+            
+            await ReplyAsync(embed: embed.Build());
         }
     }
 }
