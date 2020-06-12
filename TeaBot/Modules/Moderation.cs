799dc522cf32b6f60374d265771b7cf06ecd282d
Carrak <51882360+Carrak@users.noreply.github.com>
2020-06-13 02:42:01 +0400

    New module and a command

---
+++ TeaBot/Modules/Moderation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using TeaBot.Commands;
+
+namespace TeaBot.Modules
+{
+    [Summary("Commands meant for moderating the server.")]
+    public class Moderation : TeaInteractiveBase
+    {
+        [Command("purge")]
+        [Summary("Delete the last `count` messages. There is a hard limit of 100")]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        [RequireContext(ContextType.Guild)]
+        public async Task Purge(int count)
+        {
+            count = Math.Min(100, count);
+            var channel = Context.Channel as ITextChannel;
+            var messages = await channel.GetMessagesAsync(count).FlattenAsync();
+            await channel.DeleteMessagesAsync(messages);
+        }
+    }
+}
