d18730f37ec5f50b2714e810edb28598813a360e
Carrak <51882360+Carrak@users.noreply.github.com>
2020-06-14 02:18:46 +0400

    Created an attribute used for marking modules that cannot be disabled

---
+++ TeaBot/Attributes/EssentialModuleAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeaBot.Attributes
+{
+    /// <summary>
+    ///     Used for marking modules that cannot be disabled in guilds
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    class EssentialModuleAttribute : Attribute
+    {
+    }
+}
