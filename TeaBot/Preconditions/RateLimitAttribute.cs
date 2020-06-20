c73e3298670acf9f75735f7d930bb4b14625e530
Carrak <51882360+Carrak@users.noreply.github.com>
2020-06-20 19:26:50 +0400

    Removed most of the unused functionality

--- TeaBot/Preconditions/RateLimitAttribute.cs
+++ TeaBot/Preconditions/RateLimitAttribute.cs
@@ -23,55 +23,26 @@ namespace TeaBot.Preconditions
     public class RatelimitAttribute : PreconditionAttribute
     {
         private readonly uint _invokeLimit;
-        private readonly bool _noLimitInDMs;
-        private readonly bool _noLimitForAdmins;
-        private readonly bool _applyPerGuild;
         private readonly TimeSpan _invokeLimitPeriod;
-        private readonly ConcurrentDictionary<(ulong, ulong?), CommandTimeout> _invokeTracker = new ConcurrentDictionary<(ulong, ulong?), CommandTimeout>();
-
-        public RatelimitFlags RatelimitFlags { get; set; } = RatelimitFlags.None;
+        private readonly ConcurrentDictionary<ulong, CommandTimeout> _invokeTracker = new ConcurrentDictionary<ulong, CommandTimeout>();
 
         /// <param name="times">
         ///     The number of times a user may use the command within a certain period.
         /// </param>
-        /// <param name="period">
+        /// <param name="seconds">
         ///     The amount of time since first invoke a user has until the limit is lifted.
         /// </param>
-        /// <param name="measure">
-        ///     The scale in which the <paramref name="period"/> parameter should be measured.
-        /// </param>
-        /// <param name="flags">
-        ///     Flags to set behavior of the ratelimit.
-        /// </param>
-        public RatelimitAttribute(double period, Measure measure, uint times = 1)
+        public RatelimitAttribute(double seconds, uint times = 1)
         {
             _invokeLimit = times;
-            _noLimitInDMs = (RatelimitFlags & RatelimitFlags.NoLimitInDMs) == RatelimitFlags.NoLimitInDMs;
-            _noLimitForAdmins = (RatelimitFlags & RatelimitFlags.NoLimitForAdmins) == RatelimitFlags.NoLimitForAdmins;
-            _applyPerGuild = (RatelimitFlags & RatelimitFlags.ApplyPerGuild) == RatelimitFlags.ApplyPerGuild;
-
-            _invokeLimitPeriod = measure switch
-            {
-                Measure.Seconds => TimeSpan.FromSeconds(period),
-                Measure.Days => TimeSpan.FromDays(period),
-                Measure.Hours => TimeSpan.FromHours(period),
-                Measure.Minutes => TimeSpan.FromMinutes(period),
-                _ => throw new ArgumentOutOfRangeException(paramName: nameof(period),
-                    message: "Argument was not within the valid range.")
-            };
+            _invokeLimitPeriod = TimeSpan.FromSeconds(seconds);
         }
 
         /// <inheritdoc/>
         public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
-            if (_noLimitInDMs && context.Channel is IPrivateChannel)
-                return Task.FromResult(PreconditionResult.FromSuccess());
-
-            if (_noLimitForAdmins && context.User is IGuildUser gu && gu.GuildPermissions.Administrator)
-                return Task.FromResult(PreconditionResult.FromSuccess());
-
             DateTime now = DateTime.UtcNow;
-            (ulong, ulong?) key = _applyPerGuild ? (context.User.Id, context.Guild?.Id) : (context.User.Id, null);
+            ulong key = context.User.Id;
 
             CommandTimeout timeout = _invokeTracker.TryGetValue(key, out CommandTimeout t)
                 && now - t.FirstInvoke < _invokeLimitPeriod
@@ -99,6 +70,7 @@ namespace TeaBot.Preconditions
             }
         }
 
+
         private sealed class CommandTimeout
         {
             public uint TimesInvoked { get; set; }
@@ -110,44 +82,6 @@ namespace TeaBot.Preconditions
                 FirstInvoke = timeStarted;
             }
         }
-    }
-
-    /// <summary>
-    ///     Determines the scale of the period parameter.
-    /// </summary>
-    public enum Measure
-    {
-        Days,
-        Hours,
-        Minutes,
-        Seconds
-    }
-
-    /// <summary>
-    ///     Determines the behavior of the <see cref="RatelimitAttribute"/>.
-    /// </summary>
-    [Flags]
-    public enum RatelimitFlags
-    {
-        /// <summary>
-        ///     Set none of the flags.
-        /// </summary>
-        None = 0,
-
-        /// <summary>
-        ///     Set whether or not there is no limit to the command in DMs.
-        /// </summary>
-        NoLimitInDMs = 1 << 0,
-
-        /// <summary>
-        ///     Set whether or not there is no limit to the command for guild admins.
-        /// </summary>
-        NoLimitForAdmins = 1 << 1,
-
-        /// <summary>
-        ///     Set whether or not to apply a limit per guild.
-        /// </summary>
-        ApplyPerGuild = 1 << 2
     }
 }
 
