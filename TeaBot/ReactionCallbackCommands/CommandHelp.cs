8a051b433d17e1bdfc0eba84c178be93796ba22d
Carrak <51882360+Carrak@users.noreply.github.com>
2020-06-24 00:58:23 +0400

    Command help now displays parameters and their descriptions

--- TeaBot/ReactionCallbackCommands/CommandHelp.cs
+++ TeaBot/ReactionCallbackCommands/CommandHelp.cs
@@ -40,6 +40,7 @@ namespace TeaBot.ReactionCallbackCommands
             embed.WithTitle($"{cmd.Name} {string.Join(' ', cmd.Parameters.Select(x => $"[{x.Name}]"))}")
                 .WithDescription($"Module [{cmd.Module.Name}]")
                 .AddField("Description", cmd.Summary ?? "No description for this command yet!")
+                .AddField("Parameters", cmd.Parameters.Count > 0 ? string.Join("\n", cmd.Parameters.Select(param => $"`{param.Name}`{(param.IsOptional ? " [Optional]" : "")} - {param.Summary ?? "No description for this parameter yet!"}")) : "This command does not have any parameters")
                 .WithColor(TeaEssentials.MainColor);
 
             embed = SetDefaultFooter(embed);
