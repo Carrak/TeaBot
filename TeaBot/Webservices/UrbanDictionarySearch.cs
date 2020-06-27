2b5df3c57bea50ab7ed211e4a9f9ac6615cc3325
Carrak <51882360+Carrak@users.noreply.github.com>
2020-06-28 02:07:21 +0400

    Order definitions by thumbs up

--- TeaBot/Webservices/UrbanDictionarySearch.cs
+++ TeaBot/Webservices/UrbanDictionarySearch.cs
@@ -26,7 +26,7 @@ namespace TeaBot.Webservices
                 return Enumerable.Empty<UrbanDictionaryDefinition>();
 
             IEnumerable<UrbanDictionaryDefinition> definitions = JsonConvert.DeserializeObject<IEnumerable<UrbanDictionaryDefinition>>(jobj.ToString());
-            definitions = definitions.Where(x => x.Definition.Length < 2048).Where(x => x.Example.Length < 2048).Take(10);
+            definitions = definitions.Where(x => x.Definition.Length < 2048 && x.Example.Length < 2048).OrderByDescending(x => x.ThumbsUp);
 
             foreach (var definition in definitions)
             {
