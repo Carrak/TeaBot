0adf8b306d91070afdeeca9eab608c45794510ec
Carrak <51882360+Carrak@users.noreply.github.com>
2020-07-07 23:48:34 +0400

    Created EnumerableUtilities

---
+++ TeaBot/Utilities/EnumerableUtilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeaBot.Utilities
+{
+    /// <summary>
+    ///     Static utility class for <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class EnumerableUtilities
+    {
+        /// <summary>
+        ///     Shortens an <see cref="IEnumerable{T}"/> such that the sum of the lengths of its elements is below <paramref name="characterLimit"/>.
+        /// </summary>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to shorten.</param>
+        /// <param name="characterLimit">The limit of characters.</param>
+        /// <param name="collectionSplitter">A combination of characters used for joining the collection.</param>
+        /// <returns>The shortened collection.</returns>
+        public static IEnumerable<string> Shorten(this IEnumerable<string> collection, int characterLimit, string collectionSplitter)
+        {
+            int countToTake;
+            int totalLength = 0;
+
+            for (countToTake = 0; countToTake < collection.Count() && totalLength + countToTake * collectionSplitter.Length < characterLimit; countToTake++)
+                totalLength += collection.ElementAt(countToTake).Length;
+
+            return collection.Take(countToTake);
+        }
+
+        /// <summary>
+        ///     Shortens an <see cref="IEnumerable{T}"/> such that the sum of the lengths of its elements is below <paramref name="characterLimit"/>.
+        /// </summary>
+        /// <param name="collection">The <see cref="IEnumerable{T}"/> to shorten.</param>
+        /// <param name="stringConversionFunc">The function to use to convert <typeparamref name="T"/> to <see cref="string"/>.</param>
+        /// <param name="characterLimit">The limit of characters.</param>
+        /// <param name="collectionSplitter">A combination of characters used for joining the collection.</param>
+        /// <returns>The shortened collection.</returns>
+        public static IEnumerable<string> Shorten<T>(this IEnumerable<T> collection, Func<T, string> stringConversionFunc, int characterLimit, string collectionSplitter)
+        {
+            return collection.Select(stringConversionFunc).Shorten(characterLimit, collectionSplitter);
+        }
+    }
+}
