8205ac2669a836fbdb96027d6f8d741cf5b27802
Carrak <51882360+Carrak@users.noreply.github.com>
2020-08-16 09:45:46 +0400

    Deafen input in polls

--- TeaBot/Modules/General.cs
+++ TeaBot/Modules/General.cs
@@ -181,7 +181,7 @@ namespace TeaBot.Modules
             string[] entriesArr = null;
             var entriesMsg = await NextMessageWithConditionAsync(x =>
             {
-                entriesArr = Regex.Split(x.Content, @"\s+\|\s+");
+                entriesArr = Regex.Split(x.Content.DeafenMentionsFromMessage(x), @"\s+\|\s+");
                 int count = entriesArr.Count();
                 return count >= 2 && count <= 9;
             }, Context, TimeSpan.FromSeconds(30), errorMessage: "You've input less than 2 or more than 9 entries. Try again.");
