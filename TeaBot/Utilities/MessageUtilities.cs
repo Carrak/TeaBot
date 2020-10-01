77ef06c279da8fcfab94d3062be4cfd08167a13a
Carrak <51882360+Carrak@users.noreply.github.com>
2020-10-01 19:53:16 +0400

    Method for retrieving a message from a link

--- TeaBot/Utilities/MessageUtilities.cs
+++ TeaBot/Utilities/MessageUtilities.cs
@@ -30,3 +30,42 @@ namespace TeaBot.Utilities
             return text;
         }
 
+        public async static Task<IMessage> ParseMessageFromLinkAsync(SocketCommandContext context, string link)
+        {
+            string pattern = @"(http|https?:\/\/)?(www\.)?(discord\.(gg|io|me|li|com)|discord(app)?\.com\/channels)\/(?<Guild>\w+)\/(?<Channel>\w+)\/(?<Message>\w+)";
+            var match = System.Text.RegularExpressions.Regex.Match(link, pattern);
+
+            // Check if the given link matches the pattern
+            if (!match.Success)
+                throw new FormatException("The given link does not match Discord message link pattern - `https://discordapp.com/channels/{guild}/{channel}/{id}`");
+
+            // Check if the guild in the link is valid
+            if (!ulong.TryParse(match.Groups["Guild"].Value, out var guildid))
+                throw new FormatException("Invalid guild ID.");
+
+            // Check if the channel is valid
+            if (!ulong.TryParse(match.Groups["Channel"].Value, out var channelid))
+                throw new FormatException("Invalid channel ID.");
+
+            // Check if the guild is the same as this one
+            if (context.Guild.Id != guildid)
+                throw new FormatException("The specified message is in another guild.");
+
+            // Check if the message is valid
+            if (!ulong.TryParse(match.Groups["Message"].Value, out var messageid))
+                throw new FormatException("Invalid message ID.");
+            
+            // Check if the channel exists
+            if (!(context.Guild.GetTextChannel(channelid) is ITextChannel channel))
+                throw new ChannelNotFoundException($"Channel with ID `{channelid}` does not exist.");
+
+            // Return the message
+            return await channel.GetMessageAsync(messageid);
+        }
+    }
+
+    class ChannelNotFoundException : Exception 
+    { 
+        public ChannelNotFoundException(string message) : base(message) { }    
+    }
+}
